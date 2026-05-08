b74d7a13d7c03f77e5d1966c9b15d06dcf7a3d43
Niu Jiaqi <15670687020@163.com>
2026-05-08 17:18:30 +0800

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "delivery-dev-secret")
     AI_MODEL = os.getenv("AI_MODEL", "THUDM/GLM-Z1-9B-0414")
     AI_BASE_URL = os.getenv("AI_BASE_URL", "https://api.siliconflow.cn/v1/chat/completions")
-    AI_API_KEY = os.getenv("AI_API_KEY", "sk-zjbtminbeundgpuawqkkslyjlmuylcpgaemqvzjqgriecnex")
+    AI_API_KEY = os.getenv("AI_API_KEY", "*")
     DB_USER = os.getenv("DB_USER", "root")
     DB_PASSWORD = os.getenv("DB_PASSWORD", "110120119")
     DB_HOST = os.getenv("DB_HOST", "127.0.0.1")
